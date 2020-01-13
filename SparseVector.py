7b90d188cf0bb7751ee5126f98f286ffce55c535
Love Patel <patel.lo@husky.neu.edu>
2020-01-12 22:28:23 -0500

    commit

--- SparseVector.py
+++ SparseVector.py
@@ -60,19 +60,6 @@ class SparseVector(dict):
              will return a sparse vector containint x'y coordinates multiplied by s
         """        
         return self * s
-        
-    
-
-
-def estimateGrad(fun,x,delta):
-     """ Given a real-valued function fun, estimate its gradient numerically.
-     """
-     grad = SparseVector({})
-     for key in x:
-         e = SparseVector({})
-         e[key] = 1.0
-         grad[key] = (fun(x+delta * e) - fun(x))/delta
-     return grad
 
  
 
